ae8c7bffa79a706ee85389794386cee4a2c0d8c0
Nebulas-Learning <40438033+Nebulas-Learning@users.noreply.github.com>
2018-07-29 11:25:04 -0600

    Update Smart-Contract-Hello-World-Commented.js

--- Smart-Contract-Hello-World-Commented.js
+++ Smart-Contract-Hello-World-Commented.js
@@ -1,4 +1,4 @@
-//Nebulas Official Hello Worlds Script
+//Nebulas Official Hello Worlds Script - JavaScript
 class HelloWorld {//Define the class object of our script
     constructor() {// The constructor method is a special method for creating and initializing an object created within a class.
         LocalContractStorage.defineProperties(this, {//LocalContractStorage is a built in function of Nebulas
@@ -7,7 +7,6 @@ class HelloWorld {//Define the class object of our script
 		/*
 		The LocalContractStorage module provides a state trie based storage capability. It accepts string only key/pair values. 
 		All data is stored to a private state trie associated with current contract address, only the contract can access them.
-		
 		A list of built infunctions are available at: https://github.com/nebulasio/wiki/blob/master/smart_contract.md
 		*/
     }
@@ -15,7 +14,9 @@ class HelloWorld {//Define the class object of our script
         this.visitor = visitor;//Defining visitor variable as this.visitor
     }
     greetings(city) {//creating the greetings class/function that can be called by the user.
-        Event.Trigger("greetings", "Here is " + city + ". Hello World! By " + this.visitor + ".")//This is a string that we will be printing (hello world).
+        //This is a string that we will be printing (hello world).
+        //Event.Trigger("greetings", "Here is " + city + ". Hello World! By " + this.visitor + ".")//Prints via RPC - need local node.
+        return "greetings", "Here is " + city + ". Hello World! By " + this.visitor + ".";//Prints via execution - for web-wallet
     }
     who() {//creating the who class/function that can be called by the user.
         return this.visitor;//This will return the name of the visitor which is defined during deployment.
@@ -23,5 +24,3 @@ class HelloWorld {//Define the class object of our script
 }
 
 module.exports = HelloWorld;//All contracts must use module.exports. It must be the class/protocol object defined in the script. In our example, it is "HelloWorld"
-
-
